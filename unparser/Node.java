df375267cdeac5c49dc902d1180b78650a1edfdf
Enje <enje@enje-desktop.(none)>
2014-05-06 23:41:41 -0700

    second commit

--- unparser/Node.java
+++ unparser/Node.java
@@ -1,22 +1,33 @@
 import java.util.Vector;
 
 public class Node {
-	static String name;
-	static String nameOfParent;
+	String name;
+	String nameOfParent;
 	
-	static Vector<Node> children = new Vector<Node>();			//we use a vector since we don't know how many children each node has
-	static Node parent;
+	Vector<Node> children = new Vector<Node>();			//we use a vector since we don't know how many children each node has
+	Node parent;
+	Node root;
 	
-	Node(String n, String nop){
+	Node(String n){
 		name = n;
-		nameOfParent = nop;
 		children.add(null); 					//each node is considered to be a leaf initially
-		parent = null;							//parent is assumed to be null initially
+		parent = root;							//parent is assumed to be null initially
 	}
 	
-	public static void addParent(Node p){
-		parent = p;								//add parent after it's been declared
+	public void setParent(String p){
+		nameOfParent = p;
+		parent = new Node(p);								//add parent after it's been declared
 	}
 	
+	public Node getParent(){
+		return parent;
+	}
 	
+	public void setChild(String ch){
+		children.add(new Node(ch));
+	}
+	
+	public Node getChild(int i){
+		return children.get(i);
+	}
 }
