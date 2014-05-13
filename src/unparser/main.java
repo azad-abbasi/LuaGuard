5ac1b37dd9590210511c71d8a97d83afda5725da
Enje <enje@enje-desktop.(none)>
2014-05-13 16:43:48 -0700

    added main, modified Unparser

---
+++ src/unparser/main.java
@@ -0,0 +1,16 @@
+/** 
+ * created by Gabe Aron 5/13/2014 to test Unparser
+ */
+package unparser;
+
+public class main {
+	public static void main(String[] args){
+		TreeConstructor tr = new TreeConstructor("testIn1.txt");
+		tr.printTree();
+		
+		Unparser up = new Unparser(tr.getRoot());
+		up.unparse();
+		System.out.println("Unparse");
+		System.out.println(up.getCode());
+	}
+}
