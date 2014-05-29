6797b8f08e719adf86af09e8b5e3998c06254662
Isaac <rodriguez.isaac27@gmail.com>
2014-05-28 23:41:59 -0700

    check

--- src/unparser/ASTwalker.java
+++ src/unparser/ASTwalker.java
@@ -14,6 +14,7 @@ public class ASTwalker {
         TreeConstructor tree = new TreeConstructor(args[0]);
         InputReader.printToFile(args[1],tree.toString());
         tree.printTreeTokens();
+        System.out.println("");
 //        root.printTree();
 
     }
