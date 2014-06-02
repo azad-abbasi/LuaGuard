c19843aea855cfbfec4b4c0ecadf6777226d4521
areejalghamdi <areej_alghamdi@live.com>
2014-06-01 20:10:23 -0700

    obfuscator updated

--- src/obfuscator/VocabObfuscator.java
+++ src/obfuscator/VocabObfuscator.java
@@ -9,11 +9,16 @@ import java.util.*;
  */
 public class VocabObfuscator {
     Node treeRoot;
+    String type;
+    int keyNum ;
     public Map<String, String> recVars = new HashMap<String, String>();
     public Map<String,String> recFunc = new HashMap<String, String>();
     public HashSet<String> usedNames = new HashSet<String>();
-    public VocabObfuscator(Node treeRoot){
+    public VocabObfuscator(Node treeRoot, String type){
         this.treeRoot = treeRoot;
+        this.type = type;
+        Random r = new Random();
+        keyNum = r.nextInt(900);
     }
 
     public void obfuscate(){
@@ -50,7 +55,7 @@ public class VocabObfuscator {
                     if (recVars.containsKey(currentNode.getChild(i).getName())) {
                         currentNode.getChild(i).setName(recVars.get(currentNode.getChild(i).getName()));
                     } else {
-                        String randomvVar = randomVar();
+                        String randomvVar = randomVar(currentNode.getChild(i).getName());
                         recVars.put(currentNode.getChild(i).getName(), randomvVar);
                         currentNode.getChild(i).setName(randomvVar);
                     }
@@ -62,7 +67,7 @@ public class VocabObfuscator {
                         currentNode.getChild(i).setName(recFunc.get(currentNode.getChild(i).getName()));
                     }
                     else {
-                        String randomvVar = randomVar();
+                        String randomvVar = randomVar(currentNode.getChild(i).getName());
                         recFunc.put(currentNode.getChild(i).getName(), randomvVar);
                         currentNode.getChild(i).setName(randomvVar);
                     }
@@ -76,7 +81,7 @@ public class VocabObfuscator {
                 if (recVars.containsKey(currentNode.getChild(i).getName())) {
                     currentNode.getChild(i).setName(recVars.get(currentNode.getChild(i).getName()));
                 } else {
-                    String randomvVar = randomVar();
+                    String randomvVar = randomVar(currentNode.getChild(i).getName());
                     recVars.put(currentNode.getChild(i).getName(), randomvVar);
                     currentNode.getChild(i).setName(randomvVar);
                 }
@@ -88,7 +93,7 @@ public class VocabObfuscator {
                     if (recVars.containsKey(currentNode.getChild(i).getName())) {
                         currentNode.getChild(i).setName(recVars.get(currentNode.getChild(i).getName()));
                     } else {
-                        String randomvVar = randomVar();
+                        String randomvVar = randomVar(currentNode.getChild(i).getName());
                         recVars.put(currentNode.getChild(i).getName(), randomvVar);
                         currentNode.getChild(i).setName(randomvVar);
                     }
@@ -133,12 +138,50 @@ public class VocabObfuscator {
 
     }
 
-    public String randomVar(){
-        String outputString = confusingString();
-        while(usedNames.contains(outputString)){
+    public String randomVar(String s) {
+        /* Key words{ MinVocab,Reverse,XOR,ILOveUO,Boss,Confusing} = obfuName */
+        String outputString = "";
+        if (type.equals("MinVocab")) {
+            outputString = MinimumVocabObfuscation(s);
+            while (usedNames.contains(outputString)) {
+                outputString = MinimumVocabObfuscation(s);
+            }
+            usedNames.add(outputString);
+        } else if (type.equals("Reverse")) {
+            outputString = StringreverseObfuscation(s);
+            while (usedNames.contains(outputString)) {
+                outputString = StringreverseObfuscation(s);
+            }
+            usedNames.add(outputString);
+        } else if (type.equals("XOR")) {
+            outputString = XORObfuscation(s);
+            while (usedNames.contains(outputString)) {
+                outputString = XORObfuscation(s);
+            }
+            usedNames.add(outputString);
+        } else if (type.equals("ILOveUO")) {
+            outputString = ILOveOUObfuscation(s);
+            while (usedNames.contains(outputString)) {
+                outputString = ILOveOUObfuscation(s);
+            }
+            usedNames.add(outputString);
+
+        }else if(type.equals("Boss")){
+            outputString = BossObfuscation(s);
+            while (usedNames.contains(outputString)) {
+                outputString = BossObfuscation(s);
+            }
+            usedNames.add(outputString);
+        }else if(type.equals("Confusing")) {
             outputString = confusingString();
+            while (usedNames.contains(outputString)) {
+                outputString = confusingString();
+            }
+            usedNames.add(outputString);
         }
-        usedNames.add(outputString);
+
+
+
         return outputString;
     }
 
@@ -191,9 +234,7 @@ public class VocabObfuscator {
      */
 
     public String XORObfuscation(String w){
-        Random r = new Random();
-
-        int key = r.nextInt(900)%32;  // the key used in XOR encryption
+        int key = keyNum%32;  // the key used in XOR encryption
 
         StringBuilder result = new StringBuilder(w.length()); //obfuscated word
 
@@ -216,7 +257,7 @@ public class VocabObfuscator {
      */
 
     public String ILOveOUObfuscation(String w){
-        String s = "iloveou";
+        String s = "iloveuo";
         StringBuilder result = new StringBuilder(w.length()); //obfuscated word
         result.append('_');
         for(int i = 0 ; i < w.length() ; i++){
@@ -226,6 +267,40 @@ public class VocabObfuscator {
         }
         return result.toString();
     }
+
+
+
+	/*
+     * BossObfuscation
+     * this obfuscate uses a random number to chose which Obfuscation function to use
+     * it is a union of obfuscation functions
+     * Input: String W
+     * Output: String result: obfuscated string
+     *
+     */
+
+    public String BossObfuscation(String w){
+        int a = (int)(1+ Math.random()*1000);
+        System.out.println(a );
+        StringBuilder result = new StringBuilder(w.length());
+        switch(a%4)
+        {
+            case 0:
+                result.append(MinimumVocabObfuscation(w));
+                break;
+            case 1:
+                result.append(ILOveOUObfuscation(w));
+                break;
+            case 2:
+                result.append(StringreverseObfuscation(w));
+                break;
+            case 3:
+                result.append(XORObfuscation(w));
+                break;
+        }
+        return result.toString();
+    }
+
 
 
 }
